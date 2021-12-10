a3cf4ee4702a3e83a1020a41b4bd2c695647478b
kautsar122 <95910771+kautsar122@users.noreply.github.com>
2021-12-10 16:44:27 +0700

    Add files via upload

---
+++ 3_XI RPL 4_PHP.php
@@ -0,0 +1,16 @@
+3_XI RPL 4_PHP
+<!DOCTYPE html>
+<html>
+<head>
+<title>Belajar PHP - 3 </title>
+</head>
+<body>
+<?php
+$nama = array("kautsar"=>"xi rpl 4";
+foreach($nama as $x => $x_value) {
+    echo "nama=" . $x . ", kelas=" . $x_value;
+    echo "<br>";
+}
+?>
+</body>
+</html>
